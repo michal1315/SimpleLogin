06eab638de6885399da13605e91e1aa28f6ac2e1
michal <michal1315_1992@o2.pl>
2021-05-16 21:37:43 +0200

    Small changes in language file.

--- messages.py
+++ messages.py
@@ -3,13 +3,13 @@ def show_message(text):
 
 
 hello_txt = "Witaj w systemie logowania"
-login_txt = "Wprowadż login"
-pass_txt = "Wprowadż hasło"
+login_txt = "Wprowadż login:"
+pass_txt = "Wprowadż hasło:"
 good_credential = "Zalogowano"
 bad_credential = "Nie poprawny login lub hasło!"
 create_account_answer = "Utworzyć nowe konto?(Y/N)"
 create_account_yes = "Witamy w systemie rejestracji"
 create_account_no = "Do zobaczenia nastepnym razem"
-create_account_log = "Prosze podać login"
-create_account_pass = "Prosze podać hasło"
-create_account_finish = "Konto zostało dodane, prosze uruchomić program ponownie, Dziękujemy."
+create_account_log = "Proszę podać login:"
+create_account_pass = "Proszę podać hasło:"
+create_account_finish = "Konto zostało dodane, proszę uruchomić program ponownie, Dziękujemy."
